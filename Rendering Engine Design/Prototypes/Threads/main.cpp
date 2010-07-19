79b7bf5004efa5e91c0c5e8f9f40fa48a6323dbf
adamparko@hotmail.com <adamparko@hotmail.com@61cf6ddd-9e3f-7e79-f35f-b1a79124a90b>
2010-07-19 14:04:20 +0000

    Added a basic Mutex class

--- Rendering Engine Design/Prototypes/Threads/main.cpp
+++ Rendering Engine Design/Prototypes/Threads/main.cpp
@@ -1,31 +1,45 @@
 #include "ArgonThread.h"
+#include "ArgonMutex.h"
 
 #include "stdio.h"
 
-class lol : public Argon::ThreadTarget
+Argon::Mutex GlobalMutex;
+
+void printSomething(unsigned long input)
+{
+	GlobalMutex.Lock();
+	printf("%d", input);
+	GlobalMutex.Unlock();
+}
+
+class testThread : public Argon::ThreadTarget
 {
 public:
 	void Method()
 	{
 		for (int i = 0; i < 10000000; ++i)
 		{
-			printf("%X", rand());
+			printSomething(rand());
 		}
 	}
 };
 
 int main()
 {
-	lol rofl;
-	Argon::Thread a;
-	a.Start(&rofl);
+	testThread
+		thread1,
+		thread2;
 
-	for (int i = 0; i < 10; ++i)
+	Argon::Thread
+		a,
+		b;
+
+	a.Start(&thread1);
+	b.Start(&thread2);
+
+	for (int i = 0; i < 10000000; ++i)
 	{
-		Sleep(500);
-		a.Pause();
-		Sleep(500);
-		a.Resume();
+		printSomething(rand());
 	}
 
 	return 0;
