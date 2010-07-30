586043f150d65511784727407941242a52a79a0d
MitchelLewisBox <MitchelLewisBox@61cf6ddd-9e3f-7e79-f35f-b1a79124a90b>
2010-07-30 23:03:35 +0000

    List working In Vector Project

--- Rendering Engine Design/Prototypes/Vector/Vector/main.cpp
+++ Rendering Engine Design/Prototypes/Vector/Vector/main.cpp
@@ -1,4 +1,5 @@
-#include "Vector.h"
+#include "ArgonVector.h"
+#include "ArgonList.h"
 
 #include <iostream>
 
@@ -6,13 +7,20 @@ int main()
 {
 
 	Argon::Vector< int > pp;
+	Argon::List< int > ll;
 
 	for(int i = 0; i < 500; ++i)
 	{
 		pp.Push_Back(i);
+		ll.Push_Front( i );
 	}
 
 	for(Argon::Vector< int >::Iterator it = pp.Begin(); it != pp.End(); ++it)
+	{
+		std::cout << *it << std::endl;
+	}
+
+	for(Argon::List< int >::Iterator it = ll.Begin(); it != ll.End(); ++it)
 		std::cout << *it << std::endl;
 
 	return 0;
