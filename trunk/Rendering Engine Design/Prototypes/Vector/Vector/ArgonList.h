c80f7952a25c42fb36fe9914edd58c2346f072f7
MitchelLewisBox <MitchelLewisBox@61cf6ddd-9e3f-7e79-f35f-b1a79124a90b>
2010-07-30 23:03:35 +0000

    List working In Vector Project

--- trunk/Rendering Engine Design/Prototypes/Vector/Vector/ArgonList.h
+++ trunk/Rendering Engine Design/Prototypes/Vector/Vector/ArgonList.h
@@ -15,7 +15,7 @@ namespace Argon
 		///	Creates an empty list for elements of type T.
 		///
 		///No Params:
-		List() : m_Size( 0 )
+		List() : m_Size( 0 ), m_Data( 0x0 )
 		{
 		}
 
@@ -38,7 +38,7 @@ namespace Argon
 		/// Get the total amount of elements in the list
 		///
 		///No Params:
-		size_t Size()
+		inline size_t Size()
 		{
 			return m_Size;
 		}
@@ -48,7 +48,7 @@ namespace Argon
 		/// True is empty; used instead of Size() == 0
 		///
 		///No Params:
-		bool Empty()
+		inline bool Empty()
 		{
 			if(m_Size == 0)
 				return true;
@@ -60,7 +60,7 @@ namespace Argon
 		///Clear entire list
 		///
 		///No Params:
-		void Clear()
+		inline void Clear()
 		{
 			if(m_Size > 0)
 			{
@@ -69,35 +69,56 @@ namespace Argon
 			}
 		}
 
-		T& Front()
+		///MERGEWITH(VOID)
+		///
+		///Merge this list with anouther list at the end of this list
+		///
+		///Params MergeWith: will add all elements to the end of this list
+		void Merge( List<T> MergeWith)
+		{
+			size_t NewSize = MergeWith.m_Size + m_Size;
+			T* NewData = new T[ NewSize ];
+			T* OldData = m_Data;
+
+
+			for(size_t Index = 0; Index < m_Size; ++Index, ++NewData, ++OldData)
+				*NewData = *OldData; //Copy Across old Data first
+
+			for(size_t Index = 0; Index < MergeWith.m_Size; ++Index, ++NewData, ++MergeWith.m_Data)
+				*NewData = *MergeWith.m_Data; //Copy Across old Data first
+		}
+
+		inline T& Front()
 		{
 			if(m_Size > 0)
 				return m_Data[0];
 			return 0x0;
 		}
 
-		T& Back()
+		inline T& Back()
 		{
 			if(m_Size > 0)
 				return m_Data[m_Size];
 			return 0x0;
 		}
 
-		void Push_Front(T Item)
+		inline void Push_Front(T Item)
 		{
-			T Data = new T[++m_Size];
-			T OldData = m_Data;
+			T* Data = new T[++m_Size];
+			T* OldData = m_Data;
 
 			Data[0] = Item;
 
 			for(size_t nIndex = 1; nIndex < m_Size; ++nIndex)
 				Data[nIndex] = OldData[nIndex-1];
 
-			delete[] m_Data;
+			if( m_Data )
+				delete[] m_Data;
+
 			m_Data = Data;
 		}
 
-		void Push_Back(T Item)
+		inline void Push_Back(T Item)
 		{
 			T Data = new T[++m_Size]
 			T OldData = m_Data;
@@ -109,7 +130,7 @@ namespace Argon
 			m_Data = Data
 		}
 
-		void Pop_Front()
+		inline void Pop_Front()
 		{
 			T Data = new T[m_Size-1];
 			T OldData = m_Data;
@@ -118,11 +139,12 @@ namespace Argon
 				Data[nIndex-1] = OldData[nIndex];
 
 			--m_Size;
-			delete[] m_Data;
+			if( m_Data )
+				delete[] m_Data;
 			m_Data = Data;
 		}
 
-		void Pop_Back()
+		inline void Pop_Back()
 		{
 			T Data = new T[--m_Size];
 			T OldData = m_Data;
@@ -133,6 +155,80 @@ namespace Argon
 			delete[] m_Data;
 			m_Data = Data;
 		}
+
+		Iterator Erase(Iterator Where)
+		{
+			//Check if earasing from the end
+			if(Where == m_Data + m_Size + 1)
+				--m_Size; //at the end remove the last element
+			else //Dont use memmove use all custom functions
+			{
+				//Copy the block
+				T* Data = new T[m_AllocatedSize];
+				T* OldData = m_Data;
+
+				for(size_t Index = 0; Index < m_Size; ++Data, ++OldData, ++Index) //Copy the old data into the new buffer
+				{
+					if(*Data != Where)
+						*Data = *OldData; //Copy Data across
+				}
+				++Data = Where;
+
+				delete[] m_Data; //Delete the old data
+				m_Data = Data;
+				--m_Size; //Remove the last element
+			}
+
+		}
+
+		Iterator Insert(Iterator Where, const T& Insert)
+		{
+			if(T != 0x0)
+			{
+				size_t NewSize = ++m_Size;
+				if(NewSize > m_AllocatedSize)
+				{
+					m_AllocatedSize *= 2 + 1;
+				}
+				else
+				{
+					T* Data = new T[m_AllocatedSize];
+					T* OldData = m_Data;
+					for(size_t Index = 0; Index < m_Size; ++Data, ++OldData, ++Index) //Copy the old data into the new buffer
+					{
+						if(*Data == Where)
+						{
+							*Data = Insert; //Copy Data across
+							--OldData; --Index; //DeIncrement
+						}
+						else
+							*Data = *OldData;
+					}
+				}
+			}
+		}
+
+
+		inline Iterator Begin()
+		{
+			return m_Data;
+		}
+
+		inline Const_Iterator Begin() const
+		{
+			return m_Data;
+		}
+
+		inline Iterator End()
+		{
+			return (m_Data + m_Size);
+		}
+
+		inline Const_Iterator End() const
+		{
+			return (m_Data + m_Size);
+		}
+
 
 		List< T >& operator = (List< T > &RightHand)
 		{
