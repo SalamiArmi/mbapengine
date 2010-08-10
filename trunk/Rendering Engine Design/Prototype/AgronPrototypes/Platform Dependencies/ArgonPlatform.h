16815355da568eaefe70bd52a26da8ceb9790144
MitchelLewisBox <MitchelLewisBox@61cf6ddd-9e3f-7e79-f35f-b1a79124a90b>
2010-08-10 10:31:06 +0000

    New Prototypes Platform

---
+++ trunk/Rendering Engine Design/Prototype/AgronPrototypes/Platform Dependencies/ArgonPlatform.h
@@ -0,0 +1,13 @@
+#ifndef _ARGONPLATFORM_HEADER_
+#define _ARGONPLATFORM_HEADER_
+
+#if defined Argon_x64 //Argon x64 Platform
+	typedef __w64 unsigned long ulong;
+#else //Argon_x86
+	typedef unsigned long ulong;
+#endif
+
+#if defined ArgonExport
+#endif
+
+#endif //_ARGONPLATFORM_HEADER_
